d56eb117f0a2bef5d70ac72b59030e498b58e8a9
ollicamp <43586158+ollicamp@users.noreply.github.com>
2018-11-28 11:30:32 -0800

    classwork

---
+++ entropy_cond.py
@@ -0,0 +1,38 @@
+
+def condEntropy(x,y):
+    
+    """
+    conditional entropy, or the average entropy of x given each y, or Hxy
+    1) For all Y {i=1:numel(X)}, compute the entropy of X given each Y
+    2) Multiply H(X|Y==i) with the probability of each Y (i.e. pxi)
+    3) Sum over all i
+
+    Args:
+        x (int): array of discrete values
+        y (int): array of discrete values
+        
+    Returns:
+        Hxy (float): average conditional entropy of x given y
+
+    """
+
+    Hxy=0
+    uniquex = np.unique(x)
+    uniquey = np.unique(y)
+
+    # loop over unique elements of y
+    for i in np.arange(len(uniquey)): 
+
+        # probability that y==y(i) (prob of each y)
+        py = np.sum(y==uniquey[i]) / N
+
+        # then loop over all possible x's to compute entropy of x at each y
+        tmp=0
+        for j in np.arange(len(uniquex)):
+            px_y = np.sum((x==uniquex[j]) & (y==uniquey[i])) / np.sum(y==uniquey[i])    # e.g. prob x==1 when y==0
+            tmp += (-( px_y * np.log2(px_y) ))                                     # entropy      
+
+        # then tally up entropy of x given each specific y multiplied by the probability of that y (py)
+        Hxy += py*tmp
+
+    return Hxy
---
+++ entropy_tot.py
@@ -0,0 +1,26 @@
+
+def entropy(x):
+    """compute entropy of discrete array x
+
+    Args:
+        x (int): array of discrete values
+
+    Returns:
+        Hx (float): entropy of x
+
+    """
+    # figure out unique values of x - can be more than just 0s, 1s
+    uniquex = np.unique(x)
+
+    Hx = 0
+    for i in np.arange(len(uniquex)):
+        # probability that x==uniquex[i]
+        px = np.sum(x==uniquex[i])/len(x)    
+
+        # check for px==0 because log2(0) = -inf
+        if px!=0:
+            Hx += (-np.sum( px * np.log2(px) ))  
+        else:
+            print('px is zero for value ', i)
+        
+    return Hx
